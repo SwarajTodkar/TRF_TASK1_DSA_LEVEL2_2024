032075b6c2626c60f346fbb49ead783e154aaf2c
SwarajTodkar <145318907+SwarajTodkar@users.noreply.github.com>
2024-09-02 18:49:13 +0530

    Update Linksum.java

--- Linksum.java
+++ Linksum.java
@@ -4,9 +4,9 @@ class Node {
     int data;
     Node next;
 
-    public Node(int data) {
-        this.data = data;
-        this.next = null;
+     Node(int x) {
+        data = x;
+        next = null;
     }
 }
 
